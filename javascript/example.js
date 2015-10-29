2e05043f1b6353306f5e38cd57148d51f76b33fa
CADBOT <charliec364@gmail.com>
2015-10-28 21:01:11 -0700

    my first commit

---
+++ javascript/example.js
@@ -0,0 +1,20 @@
+var someNumber = 12;
+var anotherNumber = someNumber + 4;
+var name = 'charlie';
+var aLocation = "university of washington";
+var quote = 'A great man once said "always do such and such"'
+console.log(someNumber);
+console.log(anotherNumber);
+console.log(quote);
+
+var firstFavFood = 'pizza';
+var secondFavFood = 'ice cream';
+
+var favoriteFoods = ['pizza', 'ice cream', 'burgers'];
+console.log(favoriteFoods);
+
+console.log(favoriteFoods[0]);
+console.log(favoriteFoods[1]);
+
+var thirdFav = favoriteFoods[2];
+console.log(thirdFav);
